84a01b0eeaedc28fbadb395c0170041668035e9d
galarto <galarto1337@yandex.ru>
2023-08-28 23:01:06 +0300

    добавлен класс DataSource

---
+++ src/main/java/database/DataSource.java
@@ -0,0 +1,84 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Logger;
+
+import static database.SqlRequests.*;
+
+public class DataSource {
+    private final static Logger LOGGER = Logger.getLogger(String.valueOf(DataSource.class));
+    private final static String URL = "jdbc:postgresql://localhost5432/postgres";
+    private final static String USERNAME = "postgres";
+    private final static String PASSWORD = "09031999";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    public static void createGuestsTable() {
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(CREATE_GUESTS_TABLE);
+        } catch (SQLException e) {
+            LOGGER.info("Table GUESTS was not created");
+
+        }
+    }
+
+    public static void createTablesTable() {
+        try (Connection connection = getConnection();
+            Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(CREATE_TABLES_TABLE);
+        } catch (SQLException e) {
+            LOGGER.info("Table TABLES was not created");
+
+        }
+    }
+
+    public static void createReservationsTable() {
+        try (Connection connection = getConnection();
+            Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(CREATE_RESERVATIONS_TABLE);
+        } catch (SQLException e) {
+            LOGGER.info("Table RESERVATIONS was not created");
+
+        }
+    }
+
+    public static void dropGuestsTable() {
+        try (Connection connection = getConnection();
+            Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(DROP_GUESTS_TABLE);
+        } catch (SQLException e) {
+            LOGGER.info("Table GUESTS was not deleted");
+        }
+    }
+
+    public static void dropTablesTable() {
+        try (Connection connection = getConnection();
+            Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(DROP_TABLES_TABLE);
+        } catch (SQLException e) {
+            LOGGER.info("Table TABLES was not deleted");
+        }
+    }
+
+    public static void dropReservationsTable() {
+        try (Connection connection = getConnection();
+            Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(DROP_RESERVATIONS_TABLE);
+        }catch (SQLException e) {
+            LOGGER.info("Table RESERVATIONS was not deleted");
+        }
+    }
+
+}
---
+++ src/main/java/database/SqlRequests.java
@@ -0,0 +1,50 @@
+package database;
+
+public class SqlRequests {
+    public static final String CREATE_RESERVATIONS_TABLE =
+            "CREATE TABLE IF NOT EXISTS GUESTS (" +
+            "ID INT PRIMARY KEY," +
+            "TABLE_ID INT REFERENCES TABLES (ID) ON DELETE CASCADE ON UPDATE CASCADE," +
+            "GUEST_ID INT REFERENCES GUESTS (ID) ON DELETE CASCADE ON UPDATE CASCADE," +
+            "RESERVATION_DATE_START TIMESTAMP," +
+            "RESERVATION_DATE_END);";
+
+    public static final String CREATE_TABLES_TABLE =
+            "CREATE TABLE IF NOT EXISTS TABLE (" +
+            "ID SERIAL PRIMARY KEY," +
+            "CAPACITY INT," +
+            "IS_AVAILABLE BOOLEAN," +
+            "NUMBER INT);";
+
+    public static final String CREATE_GUESTS_TABLE =
+            "CREATE TABLE IF NOT EXISTS (" +
+            "ID SERIAL PRIMARY KEY," +
+            "NAME VARCHAR(30) NOT NULL," +
+            "SURNAME VARCHAR(30) NOT NULL," +
+            "PHONE_NUMBER VARCHAR(20) NOT NULL);";
+
+    public static final String DROP_RESERVATIONS_TABLE =
+            "DROP TABLE IF EXISTS RESERVATIONS";
+
+    public static final String DROP_TABLES_TABLE =
+            "DROP TABLE IF EXISTS TABLES CASCADE";
+
+    public static final String DROP_GUESTS_TABLE =
+            "DROP TABLE IF EXISTS GUESTS CASCADE";
+
+    public static final String ADD_RESERVATION =
+            "INSERT INTO " +
+                    "RESERVATIONS(ID, TABLE_ID, GUEST_ID, RESERVATION_DATE_START, RESERVATION_DATE_END)\r?\n" +
+                    "VALUES(?,?,?,?,?)";
+
+    public static final String ADD_TABLE =
+            "INSERT INTO " +
+                    "TABLES(CAPACITY, IS_AVAILABLE, NUMBER)\r?\n" +
+                    "VALUES(?,?,?)";
+
+    public static final String ADD_GUEST =
+            "INSERT INTO " +
+                    "GUESTS(NAME, SURNAME, PHONE_NUMBER)\r?\n" +
+                    "VALUES(?,?,?)";
+
+}
